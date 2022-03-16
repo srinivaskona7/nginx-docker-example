274c8bc389f850772103f8d1949b256f1b5c82e4
srinivaskona7 <57325579+srinivaskona7@users.noreply.github.com>
2022-03-16 15:30:41 +0530

    Update cloud9.sh

--- cloud9.sh
+++ cloud9.sh
@@ -5,10 +5,10 @@ mkdir ~/bin
 curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.12.0/kind-linux-amd64
 chmod +x ./kind
 mv kind ~/bin
-curl -LO https://dl.k8s.io/release/v1.21.1/bin/linux/amd64/kubectl
+curl -LO https://dl.k8s.io/release/v1.23.4/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 mv kubectl ~/bin
-docker image prune -a -y
+docker image prune -a 
 kind create cluster
 
 
